156a231f6882c75a0981ecf6ef271ef9f38e4efb
Kervin Antiligando <thecebuanorealtor@gmail.com>
2026-05-04 15:51:33 +0000

    Visual edits

--- src/components/pnlt/AnnouncementBar.jsx
+++ src/components/pnlt/AnnouncementBar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 export default function AnnouncementBar() {
-  return (
-    <div className="fixed top-0 left-0 right-0 z-[60] bg-gold text-navy text-center text-xs tracking-wide py-2 font-semibold hidden">
-      Official Soft Opening on September 20, 2025
-    </div>);
+  return null;
+
+
+
 
 }
